c6e863c6827afd06ad1a1226b492cdd88ba85856
stephanos <sgt29@cornell.edu>
2017-11-24 22:59:39 -0500

    Food diary template gets dynamic data passed in.

--- app/bowel_diary.py
+++ app/bowel_diary.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, render_template
 import json
 from app.database import Base, Session, User, FoodDiaryEntry
 #Get Base db class, session here from DB.
@@ -17,7 +17,7 @@ def get_users():
 	users = session.query(User).order_by(User.id)
 	return jsonify([user.to_response() for user in users])
 
-@app.route("/users/<user_name>/food_diary", methods=["GET", "POST"])
+#@app.route("/users/<user_name>/food_diary", methods=["GET", "POST"])
 def food_diary(user_name):
 	if request.method == "POST":
 		body = request.get_json()
@@ -31,15 +31,17 @@ def food_diary(user_name):
 		diary_entries = session.query(FoodDiaryEntry).filter(FoodDiaryEntry.author_id==user.id).order_by(FoodDiaryEntry.created_at)
 		return jsonify([food_diary_entry.to_response() for food_diary_entry in diary_entries])
 
-#def food_diary_page(user_name):
+def food_diary_entries(user_name):
+	user = session.query(User).filter(User.name == user_name).first()
+	diary_entries = session.query(FoodDiaryEntry).filter(FoodDiaryEntry.author_id==user.id).order_by(FoodDiaryEntry.created_at)
+	#return jsonify([food_diary_entry.to_response() for food_diary_entry in diary_entries])
+	return diary_entries
 
-
-
-#@app.route("/users/<user_name>/food_diary/<entry_id>", methods=["GET"])
-#def get_food_diary_entry(user_name, entry_id):
-#	user = session.query(User).filter(User.name == user_name).first()
-#	entry = session.query(FoodDiaryEntry).filter(FoodDiaryEntry.author_id == user.id and FoodDiaryEntry.id == entry_id).first()
-#	return entry.to_response
+@app.route("/users/<user_name>/food_diary", methods=['GET'])
+def food_diary_page(user_name):
+	entries = food_diary_entries(user_name)
+	return render_template("food_diary.html", entries=entries)
+	#return render_template("food_diary.html", entries=entries)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
