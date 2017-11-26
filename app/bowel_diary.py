01386e2e04fe68ace57d2951b83ae43c588eb29c
stephanos <sgt29@cornell.edu>
2017-11-25 22:38:21 -0500

    Cleaned up food diary template.

--- app/bowel_diary.py
+++ app/bowel_diary.py
@@ -17,8 +17,13 @@ def get_users():
 	users = session.query(User).order_by(User.id)
 	return jsonify([user.to_response() for user in users])
 
-#@app.route("/users/<user_name>/food_diary", methods=["GET", "POST"])
-def food_diary(user_name):
+def food_diary_entries(user_name):
+	user = session.query(User).filter(User.name == user_name).first()
+	diary_entries = session.query(FoodDiaryEntry).filter(FoodDiaryEntry.author_id==user.id).order_by(FoodDiaryEntry.created_at)
+	return diary_entries
+
+@app.route("/users/<user_name>/food_diary", methods=['GET', 'POST'])
+def food_diary_page(user_name):
 	if request.method == "POST":
 		body = request.get_json()
 		content = body["content"]
@@ -27,20 +32,8 @@ def food_diary(user_name):
 		session.commit()
 		return ('', 204)
 	else:
-		user = session.query(User).filter(User.name == user_name).first()
-		diary_entries = session.query(FoodDiaryEntry).filter(FoodDiaryEntry.author_id==user.id).order_by(FoodDiaryEntry.created_at)
-		return jsonify([food_diary_entry.to_response() for food_diary_entry in diary_entries])
-
-def food_diary_entries(user_name):
-	user = session.query(User).filter(User.name == user_name).first()
-	diary_entries = session.query(FoodDiaryEntry).filter(FoodDiaryEntry.author_id==user.id).order_by(FoodDiaryEntry.created_at)
-	return diary_entries
-
-@app.route("/users/<user_name>/food_diary", methods=['GET'])
-def food_diary_page(user_name):
-	entries = food_diary_entries(user_name)
-	return render_template("food_diary.html", entries=entries)
-	#return render_template("food_diary.html", entries=entries)
+		entries = food_diary_entries(user_name)
+		return render_template("food_diary.html", entries=entries)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
