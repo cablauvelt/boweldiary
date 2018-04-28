b714d127a4bc1c1b3d4554be91d9f57eb42a87ed
stephanos <sgt29@cornell.edu>
2018-04-28 16:17:53 -0400

    Added error messages around edamam config file.

--- app/external/edamam/constants/__init__.py
+++ app/external/edamam/constants/__init__.py
@@ -1,10 +1,17 @@
 import json
 import os
 
-current_file = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-print(current_file)
-config = json.load(open(current_file + "/config.json"))
-print(json.dumps(config))
-app_key = config["app_key"]
+app_key = None
+app_id = None
 
-app_id = config["app_id"]
+current_file = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+try:
+    config = json.load(open(current_file + "/config.json"))
+except IOError:
+    msg ='Please put a file called config.json in {}'.format(current_file)
+    msg = msg + ' of the form:\n { "app_id" : <Edamam app id>, "app_key" : <Edamam app key> }'
+    raise ValueError(msg)
+else:
+    app_key = config["app_key"]
+    app_id = config["app_id"]
